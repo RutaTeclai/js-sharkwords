808a5d3ae441a796f13f3c19eb6c68bf5a19d55e
RutaTeclai <rutisyear@gmail.com>
2020-10-29 21:25:44 +0000

    Check if correct letter was clicked
    Co-authored-by: alishahwee <alisha.hwee@outlook.com>

--- static/js/sharkwords.js
+++ static/js/sharkwords.js
@@ -45,7 +45,7 @@ const disableLetterButton = (buttonEl) => {
 // Return `true` if `letter` is in the word.
 //
 const isLetterInWord = (letter) => {
-  // Replace this with your code
+  return $(".letter-box").hasClass(letter);
 };
 
 // Called when `letter` is in word. Update contents of divs with `letter`.
