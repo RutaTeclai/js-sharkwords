8037bfe1cb8d426b4c9f34dedba2615e79ec476e
RutaTeclai <rutisyear@gmail.com>
2020-10-29 17:00:23 +0000

    generate divs for chars and letter buttons
    Co-authored-by: alishahwee <alisha.hwee@outlook.com>

--- static/js/sharkwords.js
+++ static/js/sharkwords.js
@@ -19,13 +19,18 @@ let numWrong = 0;
 // Loop over the chars in `word` and create divs.
 //
 const createDivsForChars = (word) => {
-  // Replace this with your code
+  for (const letter of word) {
+    const div = `<div class="letter-box ${letter}"></div>`;
+    $("#word-container").append(div);
+  }
 };
 
 // Loop over each letter in `ALPHABET` and generate buttons.
 //
 const generateLetterButtons = () => {
-  // Replace this with your code
+  for (const letter of ALPHABET){
+    $("#letter-buttons").append(`<button>${letter}</button>`);
+  }
 };
 
 // Set the `disabled` property of `buttonEl` to `true.
